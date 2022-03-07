1d49ce6850545d3e17c3af4a76527401f7a441a0
Richard Wong <rwongsol@hotmail.com>
2022-03-06 20:49:59 -0500

    test driven develoment TDD

--- src/main/java/org/developer/junit5/model/Cuenta.java
+++ src/main/java/org/developer/junit5/model/Cuenta.java
@@ -1,12 +1,37 @@
 package org.developer.junit5.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Cuenta {
 
       private String persona;
       private BigDecimal saldo;
 
+
+
+      public Cuenta(String persona, BigDecimal saldo) {
+            this.persona = persona;
+            this.saldo = saldo;
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+             if (!(obj instanceof Cuenta)){
+                   return false;
+             }
+             Cuenta c = (Cuenta) obj;
+            if (this.persona == null || this.saldo == null){
+                  return false;
+            }
+
+            return this.persona.equals(c.getPersona()) && this.saldo.equals(c.getSaldo());
+      }
+
+      @Override
+      public int hashCode() {
+            return Objects.hash(persona, saldo);
+      }
 
       public String getPersona() {
             return persona;
@@ -23,4 +48,7 @@ public class Cuenta {
       public void setSaldo(BigDecimal saldo) {
             this.saldo = saldo;
       }
+
+
+
 }
--- src/test/java/org/developer/junit5/model/CuentaTest.java
+++ src/test/java/org/developer/junit5/model/CuentaTest.java
@@ -3,6 +3,8 @@ package org.developer.junit5.model;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
@@ -11,13 +13,33 @@ class CuentaTest {
     @Test
     void testNombreCuenta() {
 
-        Cuenta cuenta = new Cuenta();
+        Cuenta cuenta = new Cuenta("richard",  new BigDecimal( "1000.12345")) ;
         cuenta.setPersona("richard");
-
         String esperado = "richard";
 
         Assertions.assertEquals(esperado,cuenta.getPersona());
         assertEquals(esperado,cuenta.getPersona());
+        assertTrue(esperado.equals(cuenta.getPersona()));
     }
+
+    @Test
+    void testSaldoCuenta(){
+
+        Cuenta cuenta = new Cuenta("richard", new BigDecimal("100.12345"));
+        assertEquals(100.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO)> 0);
+    }
+
+    @Test
+    void testReferenciaCuenta(){
+
+        Cuenta cuenta = new Cuenta("Jon Doe", new BigDecimal("8900.9997"));
+        Cuenta cuenta2 = new Cuenta("Jon Doe", new BigDecimal("8900.9997"));
+
+        assertEquals(cuenta2,cuenta);
+
+    }
+
 
 }
